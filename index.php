324966c05fd6a7f39b66ce50ad7db47be8ba6434
iamcad <mhmmdirshad8@gmail.com>
2023-11-27 20:27:58 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,305 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Project POL PP</title>
+
+    <!--Font awesome cdm link-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+
+    <!--Bootstrap link-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/4.6.1/css/bootstrap.min.css">
+
+    <!--css file link-->
+    <link rel="stylesheet" href="style.css">
+
+</head>
+<body>
+
+<!-- Header section  start-->
+
+<header class="header fixed-top">
+
+    <div class="container">
+
+        <div class="row align-items-center justify-content-between">
+
+            <a href="#home" class="logo">Si<span>Duta.</span></a>
+
+            <nav class="nav">
+                <a href="#home">home</a>
+                <a href="#about">about</a>
+                <a href="#services">job</a>
+                <a href="#reviews">complaint report</a>
+                <a href="#contact">report a complaint</a>
+            </nav>
+
+            <a href="#contact" class="link-btn">report a complaint</a>
+
+            <div id="menu-btn" class="fas fa-bars"></div>
+
+        </div>
+
+    </div>
+
+</header>
+
+<!--header section ends -->
+
+<!-- home section starts -->
+
+<section class="home" id="home">
+
+    <div class="container">
+
+        <div class="row min-vh-100 align-items-center">
+            <div class="content text-center text-md-left">
+                <h3>Praja Wibawa</h3>
+                <p>Sistem Informasi Pengaduan Dan Berita</p>
+                <a href="#contact" class="link-btn">report a complaint</a>
+            </div>
+        </div>
+
+    </div>
+
+</section>
+
+<!-- home section ends -->
+
+<!-- about section starts -->
+
+<section class="about" id="about">
+
+    <div class="container">
+
+        <div class="row align-items-center">
+
+            <div class="col-md-6 image">
+                <img src="gambar/foto7.jpg" class="w-100 mb-5 mb-md-0" alt="">
+            </div>
+
+            <div class="col-md-6 content">
+                <span>about </span>
+                <h3>Siap Melayani Pertolongan Anda</h3>
+                <p>web SiDuta (situs Informasi Pengaduan Dan Berita) adalah situs untuk menjembatani laporan masyarakat terkait ketertiban umum yang berada di lingkungan masyarakat</p>
+                <a href="#contact" class="link-btn">make appointment</a>
+            </div>
+
+        </div>
+
+    </div>
+
+</section>
+
+<!-- about section ends -->
+
+<!-- services section starts -->
+
+<section class="services" id="services">
+
+   <h1 class="heading">job</h1>
+
+    <div class="box-container container">
+
+        <div class="box">
+            <img src="gambar/pencuri.jpg" alt="Image" height="42" width="42">
+            <h3>Tugas 1</h3>
+            <p>Penyusunan program dan pelaksanaan penegakkan peraturan daerah, penyelenggaraan ketertiban umum dan ketentraman masyarakat serta perlindungan masyarakat</p>     
+        </div>
+
+        <div class="box">
+            <img src="gambar/borgol.png" alt="Image" height="42" width="65">
+            <h3>Tugas 2 </h3>
+            <p>Pelaksanaan kebijakan penegakkan peraturan daerah dan peraturan kepala daerah</p>     
+        </div>
+
+        <div class="box">
+            <img src="gambar/penjara.png" alt="Image" height="42" width="50">
+            <h3>Tugas 3</h3>
+            <p>Pelaksanaan kebijakan penyelenggaraan ketertiban umum dan ketentraman masyarakat di daerah</p>     
+        </div>
+
+        <div class="box">
+            <img src="gambar/lapas.jpg" alt="Image" height="42" width="42">
+            <h3>Tugas 4</h3>
+            <p>Pelaksanaan kebijakan perlindungan masyarakat di daerah</p>     
+        </div>
+
+        <div class="box">
+            <img src="gambar/pentungan.jpg" alt="Image" height="60" width="60">
+            <h3>Tugas 5</h3>
+            <p>Pelaksanaan koordinasi penegakkan peraturan daerah dan peraturan kepala daerah, penyelenggaraan ketertiban umumdan ketentraman masyarakat dengan Kepolisian Negara Republik Indonesia,</p>     
+        </div>
+
+        <div class="box">
+            <img src="gambar/pistol.png" alt="Image" height="42" width="42">
+            <h3>Tugas 6</h3>
+            <p>Pengawasan terhadap masyarakat, aparatur, atau badan hukum agar mematuhi dan mentaati peraturan daerah dan peraturan kepala daerah</p>     
+        </div>
+
+    </div>
+    
+</section>
+
+<!-- services section ends -->
+
+<!-- process section starts --> 
+
+<section class="process">
+
+    <h1 class="heading">activity</h1>
+
+    <div class="box-container container">
+
+        <div class="box">
+            <img src="gambar/foto5.jpg" alt="Image" height="200" width="300">
+            <h3>Pemberian Materi</h3>
+            <p>Pemberian Materi dari bapak ............... tentang penjagaan dan masalah yang terjadi di lingkungan masyarakat</p>
+        </div>
+
+        <div class="box">
+            <img src="gambar/foto1.jpg" alt="Image" height="200" width="300">
+            <h3>Pelaksanaan Pendidikan</h3>
+            <p>pembelajaran kepada para calon anggota tentang tugas-tugas dan kewajiban mereka kepada masyarakat</p>
+        </div>
+
+        <div class="box">
+            <img src="gambar/foto8.jpg" alt="Image" height="200" width="300">
+            <h3>Praktek Lapangan</h3>
+            <p>pelaksanaaan praktek masalah-masalah yang terjadi di lapangan agar mereka lebih siap sebelum mereka terjun langsung ke masalah yang sebenarnya</p>
+        </div>
+
+    </div>
+
+</section>
+
+<!-- process section ends -->
+
+<!-- reviews section starts -->
+
+<section class="reviews" id="reviews">
+
+    <h1 class="heading"> complaint report </h1>
+
+    <div class="box-container container">
+
+        <div class="box">
+            <img src="gambar/icon.png" alt="">
+            <p>Laporan tentang premanisme yang terjadi di pasar parung</p>
+            <div class="stars">
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star-half-alt"></i>
+            </div>
+            <h3>riko</h3>
+            <span>satpol PP-nya tegas-tegas, keren lah pokoknya</span>
+        </div>
+
+        <div class="box">
+            <img src="gambar/icon.png" alt="">
+            <p>Laporan tentang penggunaan bahu jalan yang dipakai oleh pedagang kaki 5</p>
+            <div class="stars">
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star-half-alt"></i>
+            </div>
+            <h3>erik</h3>
+            <span>satpol PP-nya gercep banget, setelah datang masalah lalu terlerai</span>
+        </div>
+
+        <div class="box">
+            <img src="gambar/icon.png" alt="">
+            <p>Laporan kerusuhan di pasar leuwisadeng dikarenakan adanya konflik perebutan wilayah kekuasaan pasar</p>
+            <div class="stars">
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star-half-alt"></i>
+            </div>
+            <h3>james</h3>
+            <span>Pelayanan cukup baik dan sigap </span>
+        </div>
+
+    </div>
+
+</section>
+
+<!-- reviews section ends -->
+
+<!-- contact section starts -->
+
+<section class="contact" id="contact">
+
+    <h1 class="heading">report a complaint</h1>
+
+    <form>
+        <span>Nama :</span>
+        <input type="text" name="name" placeholder="masukkan nama anda" class="box" required>
+        <span>Email :</span>
+        <input type="email" name="email" placeholder="masukkan email anda" class="box" required>
+        <span>Nomor Telepon :</span>
+        <input type="number" name="number" placeholder="masukkan nomor telepon anda" class="box" required>
+        <span>Tanggal Pengaduan :</span>
+        <input type="datetime-local" name="date" class="box" required>
+        <span>Rincian Pengaduan</span>
+        <input type="text" name="name" placeholder="masukkan pengaduan anda" class="box" required>
+        <input type="button" value="report a complaint" name="submit" class="link-btn" onclick="cart()">
+    </form>
+    
+</section>
+
+<!-- contact section ends -->
+
+<!-- footer section starts -->
+
+<section class="footer">
+
+    <div class="box-container container">
+
+        <div class="box">
+            <i class="fas fa-phone"></i>
+            <h3>phone number</h3>
+            <p>+62-813-456-789-100</p>
+        </div>
+
+        <div class="box">
+            <i class="fas fa-map-marker-alt"></i>
+            <h3>our address</h3>
+            <p>Parung, Bogor - 123456</p>
+        </div>
+
+        <div class="box">
+            <i class="fas fa-clock"></i>
+            <h3>opening hours</h3>
+            <p>08:00am to 17:00pm</p>
+        </div>
+
+        <div class="box">
+            <i class="fas fa-envelope"></i>
+            <h3>email address</h3>
+            <p>rafigani@gmail.com</p>
+        </div>
+
+    </div>
+
+    <div class="credit"> &copy; copyright @ <?php echo date('Y'); ?> by <span>rafii abdul ghani</span> </div>
+    
+</section>
+
+<!-- footer section ends -->
+
+<!-- custom js file link -->
+<script src="script.js"></script>
+
+<!-- sweet alert pop-up link -->
+<script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+
+</body
+</html> 
